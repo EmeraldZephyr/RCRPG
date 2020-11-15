1d5a0aa3dcf8d68fc03f6302f00210ee96063acf
Jared Mitasky <jaredmitasky@gmail.com>
2020-11-14 22:08:47 -0500

    Updated doc. Working on character builder

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,13 +1,26 @@
 
 //Control slider
 var slider = document.getElementById("myRange");
+
 var output = document.getElementById("levelNum");
 output.innerHTML = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
+slider.oninput = function(e) {
+    e.stopPropagation();
   output.innerHTML = this.value;
+
 }
+var graceSlider = document.getElementById("grace");
+var graceNum = document.getElementById("graceNum");
+graceSlider.oninput=function(e){
+    e.stopPropagation();
+    this.max=
+       slider.value-brawnSlider.value
+    ;
+    output.innerHTML=this.value;
+};
+
 
 //Add log to action log
 const shout = (str)=>{
